cf497704fcfbca3c22368ed4c6855cef40661be8
s10case <s10case@gmail.com>
2015-01-17 19:26:19 -0800

    Initial checkin of myFLIP

---
+++ myFLIP.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myFLIP.Startup))]
+namespace myFLIP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
